fded3d10f878f0ffb7db590fd9b3096670ab3829
Mikalai Charniak <mikola.charniak@gmail.com>
2019-10-12 10:31:38 +0300

    configured core ef DbContext

---
+++ CW.TestSystem.DataProvider/DbInfrastracture/TestSystemDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using CW.TestSystem.Model.CoreEntities;
+
+
+namespace CW.TestSystem.DataProvider.DbInfrastracture
+{
+    public class TestSystemDbContext : DbContext
+    {
+        public TestSystemDbContext(DbContextOptions<TestSystemDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Result> Results { get; set; }
+    }
+}
