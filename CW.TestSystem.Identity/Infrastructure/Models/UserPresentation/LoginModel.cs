a6fc2eb66381a0f4c4e7e8e4294fd5a4d72b46a8
Mikalai Charniak <mikola.charniak@gmail.com>
2019-10-16 21:18:39 +0300

    added basic structure for account setup actions; added basic models for Login and Registration actions

---
+++ CW.TestSystem.Identity/Infrastructure/Models/UserPresentation/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CW.TestSystem.Identity.Infrastructure.Models.UserPresentation
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CW.TestSystem.Identity/Infrastructure/Models/UserPresentation/RegistrationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CW.TestSystem.Identity.Infrastructure.Models.UserPresentation
+{
+    public class RegistrationModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}
