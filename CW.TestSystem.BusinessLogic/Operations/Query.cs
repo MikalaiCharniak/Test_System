78b10692df171ff916901b56b1c225e1bd9b0e6f
Mikalai Charniak <mikola.charniak@gmail.com>
2019-11-02 11:08:05 +0300

    added single queries for root objects update gitignore

--- CW.TestSystem.BusinessLogic/Operations/Query.cs
+++ CW.TestSystem.BusinessLogic/Operations/Query.cs
@@ -2,14 +2,29 @@
 using CW.TestSystem.Model.CoreEntities;
 using HotChocolate;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CW.TestSystem.BusinessLogic.Operations
 {
     public class Query
     {
-        public Test GetTest([Service] TestSystemDbContext context,Guid id)
+        public async Task<Test> GetTestAsync([Service] TestSystemDbContext context,Guid id)
         {
-            return context.Tests.Find(id);
+            return await context.Tests.FindAsync(id);
+        }
+        public async Task<Question> GetQuestionAsync([Service] TestSystemDbContext context, Guid id)
+        {
+            return await context.Questions.FindAsync(id);
+        }
+
+        public async Task<Tag> GetTagAsync([Service] TestSystemDbContext context, Guid id)
+        {
+            return await context.Tags.FindAsync(id);
+        }
+        public async Task<Result> GetResultAsync([Service] TestSystemDbContext context, Guid id)
+        {
+            return await context.Results.FindAsync(id);
         }
     }
 }
