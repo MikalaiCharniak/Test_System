5b0fc3415f4d33c43de9683116b6701ceaf4a85d
Mikalai Charniak <mikola.charniak@gmail.com>
2019-11-03 16:56:42 +0300

    expire query type, play with filtering/paging/sorting a few

--- CW.TestSystem.BusinessLogic/Operations/Query.cs
+++ CW.TestSystem.BusinessLogic/Operations/Query.cs
@@ -1,6 +1,7 @@
 ﻿using CW.TestSystem.DataProvider.DbInfrastracture;
 using CW.TestSystem.Model.CoreEntities;
 using HotChocolate;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,22 +10,42 @@ namespace CW.TestSystem.BusinessLogic.Operations
 {
     public class Query
     {
-        public async Task<Test> GetTestAsync([Service] TestSystemDbContext context,Guid id)
+        public async Task<Test> GetTestAsync([Service] TestSystemDbContext context, Guid id)
         {
             return await context.Tests.FindAsync(id);
         }
+
+        public async Task<IEnumerable<Test>> GetTestsAsync([Service] TestSystemDbContext context)
+        {
+            return await context.Tests.ToListAsync();
+        }
+           
         public async Task<Question> GetQuestionAsync([Service] TestSystemDbContext context, Guid id)
         {
             return await context.Questions.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Question>> GetQuestionsAsync([Service] TestSystemDbContext context)
+        {
+            return await context.Questions.ToListAsync();
         }
 
         public async Task<Tag> GetTagAsync([Service] TestSystemDbContext context, Guid id)
         {
             return await context.Tags.FindAsync(id);
         }
+
+        public async Task<IEnumerable<Tag>> GetTagsAsync([Service] TestSystemDbContext context)
+        {
+            return await context.Tags.ToListAsync();
+        }
         public async Task<Result> GetResultAsync([Service] TestSystemDbContext context, Guid id)
         {
             return await context.Results.FindAsync(id);
+        }
+        public async Task<IEnumerable<Result>> GetResultsAsync([Service] TestSystemDbContext context)
+        {
+            return await context.Results.ToListAsync();
         }
     }
 }
--- CW.TestSystem.BusinessLogic/Types/Operations/QueryType.cs
+++ CW.TestSystem.BusinessLogic/Types/Operations/QueryType.cs
@@ -1,15 +1,32 @@
 ﻿using CW.TestSystem.BusinessLogic.Operations;
 using HotChocolate.Types;
-using HotChocolate.Configuration;
-using HotChocolate.Types.Descriptors.Definitions;
+using CW.TestSystem.BusinessLogic.Types.Models;
+using HotChocolate.Types.Relay;
 
 namespace CW.TestSystem.BusinessLogic.Types.Operations
 {
     public class QueryType : ObjectType<Query>
     {
-        protected override ObjectTypeDefinition CreateDefinition(IInitializationContext context)
+        protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
         {
-            return base.CreateDefinition(context);
+            descriptor.Field(x => x.GetTestsAsync(default)).
+                Type<ListType<TestType>>().
+                UsePaging<TestType>().
+                UseFiltering().
+                UseSorting().
+                Description("Get tests with filtering");
+
+            descriptor.Field(x => x.GetTagsAsync(default)).
+                Type<ListType<TestType>>().UseFiltering().
+                Description("Get tags with filtering");
+
+            descriptor.Field(x => x.GetQuestionsAsync(default)).
+                Type<ListType<TestType>>().UseFiltering().
+                Description("Get questions with filtering");
+
+            descriptor.Field(x => x.GetResultsAsync(default)).
+                Type<ListType<TestType>>().UseFiltering().
+                Description("Get results with filtering");
         }
     }
 }
